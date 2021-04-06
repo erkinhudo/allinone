66fa72089669711d23251b3115ee7d8f280114f4
Eric Hudo <erkinhudo@gmail.com>
2021-04-06 01:07:59 -0400

    Different pages was added

--- app.js
+++ app.js
@@ -15,16 +15,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",function(req,res){
+  res.render("home",{startingContent:homeStartingContent});
+});
 
+app.get("/about", function(req,res){
+  res.render("about", {aboutus:aboutContent} );
+});
 
+app.get("/contact", function (req,res){
+  res.render("contact",{contacts:contactContent});
+});
 
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
 
-
-
-
-
-
-
+app.post("/compose",function(req,res){ 
+  console.log(req.body.postTitle)
+})
 
 
 
