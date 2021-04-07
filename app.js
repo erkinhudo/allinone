69a8da5ccc72521d4d75ca3649dd1151c0669b77
Eric Hudo <erkinhudo@gmail.com>
2021-04-07 00:34:57 -0400

    Templates was added

--- app.js
+++ app.js
@@ -10,13 +10,20 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var posts = [];
+
 app.get("/",function(req,res){
-  res.render("home",{startingContent:homeStartingContent});
+  res.render("home",{
+    startingContent:homeStartingContent,
+    postContent:posts
+  });
+
 });
 
 app.get("/about", function(req,res){
@@ -31,9 +38,16 @@ app.get("/compose",function(req,res){
   res.render("compose");
 });
 
-app.post("/compose",function(req,res){ 
-  console.log(req.body.postTitle)
-})
+app.post("/compose",function(req,res){    
+  const post = {
+    title : req.body.postTitle,
+    content : req.body.postBody
+  };
+  posts.push(post);
+
+  res.redirect('/');
+  
+});
 
 
 
